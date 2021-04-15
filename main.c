cb6367e363d1638dbfc275de0247869208000cb1
Thalukanyo Ramasindi <thaluramasindi@gmail.com>
2021-04-15 11:46:23 +0200

    Refactoring code

--- main.c
+++ main.c
@@ -3,6 +3,7 @@ int main(){
 int m,n;
 int blnExceeds = 0;
 printf("Enter the number of rows and columns for matrix, eg: 2 2, 3 3 \n");
+//validating array size
 do
 {
     scanf("%i%i",&m,&n);
@@ -15,25 +16,26 @@ do
         blnExceeds = 0;
     }
 }while(blnExceeds == 1);
-
+//Variable Declarations
 int mat1[m][n];
 int mat2[m][n];
 int result[m][n];
 int i,j;
 
     printf("Enter elements of matrix A :\n");
+    //scanning user input
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
             scanf("%d",&mat1[i][j]);
         }
     }
-    printf("Enter the elements of matrix B:\n");
+    printf("Enter elements of matrix B:\n");
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
             scanf("%d",&mat2[i][j]);
         }
     }
-
+    printf("\n");
     //Print formatted matrices
     printf("Matrix A :\n");
     for(i=0;i< m;i++)
@@ -53,11 +55,13 @@ int i,j;
         }
         printf("\n");
     }
+    //computing matrices
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
             result[i][j] = mat1[i][j]+mat2[i][j];
         }
     }
+    //printing results
     printf("Addition: A + B = \n");
     for(i=0;i<m;i++){
         for(j=0;j<n;j++)
